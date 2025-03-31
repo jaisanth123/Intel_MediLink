8f3c4ba49ec47bd95a1d5a4eb1c931a79caa7fd1
jaisanth123 <jaisanth2006@gmail.com>
2025-03-31 13:07:09 +0530

    backend cors updated deploy

--- backend/node/server.js
+++ backend/node/server.js
@@ -15,7 +15,7 @@ connectDB();
 const app = express();
 
 const corsOptions = {
-  origin: "http://localhost:5173", // Replace with your frontend URL
+  origin: "*", // Replace with your frontend URL
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true, // Allow credentials (cookies, authorization headers, etc.)
 };
